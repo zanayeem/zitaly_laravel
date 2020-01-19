69964a0a8a17e35d4ce0fb3a585597e46ca11c01
Zannatun Nayeem <nayeem123able@gmail.com>
2020-01-19 20:08:29 +0600

    Initial commit

---
+++ resources/views/frontend/layout.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+    <!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>zItalyFood | Free Restaurant Html5 Templates</title>
+	<meta name="description" content="Free Responsive Html5 Css3 Templates | zerotheme.com">
+	<meta name="author" content="www.zerotheme.com">
+	
+    <!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    <!-- CSS
+  ================================================== -->
+  	<link rel="stylesheet" href="{{ asset('public/frontend/css/zerogrid.css')}}">
+	<link rel="stylesheet" href="{{ asset('public/frontend/css/style.css')}}">
+	<link rel="stylesheet" href="{{ asset('public/frontend/css/slide.css')}}">
+	<link rel="stylesheet" href="{{ asset('public/frontend/css/menu.css')}}">
+	<!-- Custom Fonts -->
+    <link href="{{ asset('public/frontend/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+	<!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+		<script src="js/html5.js"></script>
+		<script src="js/css3-mediaqueries.js"></script>
+	<![endif]-->
+    
+</head>
+<body>
+<div class="wrap-body">
+	<!--///////////////////////////////////////Top-->
+	<div class="top">
+		<div class="zerogrid">
+			<ul class="number f-left">
+				<li class="mail"><p>ContacUst@Gmail.com</p></li>
+				<li class="phone"><p>08 88888 88888</p></li>
+			</ul>
+			<ul class="top-social f-right">
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<!--////////////////////////////////////Header-->
+	<header>
+		<div class="zerogrid">
+			<center><div class="logo"><img src="public/frontend/images/logo.png"></div></center>
+		</div>
+	</header>
+	<div class="site-title">
+		<div class="zerogrid">
+			<div class="row">
+				<h2 class="t-center">Truely the best restaurant in town - The New York Times</h2>
+			</div>
+		</div>
+	</div>
+    <!--//////////////////////////////////////Menu-->
+    <a href="#" class="nav-toggle">Toggle Navigation</a>
+    <nav class="cmn-tile-nav">
+		<ul class="clearfix">
+			<li class="colour-1"><a href="{{ url('/')}}">Home</a></li>
+			<li class="colour-2"><a href="{{ route('menu')}}">Menu</a></li>
+			<li class="colour-3"><a href="{{ route('location')}}">Location</a></li>
+			<li class="colour-4"><a href="{{ route('blog')}}">Blog</a></li>
+			<li class="colour-5"><a href="{{ route('reservation')}}">Reservation</a></li>
+			<li class="colour-6"><a href="{{ route('staff')}}">Our Staff</a></li>
+			<li class="colour-7"><a href="{{ route('news')}}">News</a></li>
+			<li class="colour-8"><a href="{{ route('gallery')}}">Gallery</a></li>
+		</ul>
+    </nav>
+	
+
+	
+
+	@yield('main-content')
+
+
+	<!--////////////////////////////////////Footer-->
+<footer class="zerogrid">
+	<div class="wrap-footer">
+		<div class="row">
+			<div class="col-1-3">
+				<div class="wrap-col">
+					<h4>Customer Testimonials</h4>
+					<div class="row">
+						<img src="public/frontend/images/a-1.jpg">
+						<h5>Nick Roach</h5>
+						<p>Pellentesque elementum leo et justo dapibus convalli. In justo nibh, congue nec dapibus ac, placerat eget sem. Nunc consequat felis non elit ultricies in varius massa laoreet. Aenean lectus nisl, ellentesque in fermentum sit amet, convallis a lorem condimentum mollis. Aenean lectus nisl, ellentesque in fermentum sit amet.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-1-3">
+				<div class="wrap-col">
+					<h4>Location</h4>
+					<div class="wrap-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.289259162295!2d-120.7989351!3d37.5246781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8091042b3386acd7%3A0x3b4a4cedc60363dd!2sMain+St%2C+Denair%2C+CA+95316%2C+Hoa+K%E1%BB%B3!5e0!3m2!1svi!2s!4v1434016649434" width="100%" height="200" frameborder="0" style="border:0"></iframe></div>
+				</div>
+			</div>
+			<div class="col-1-3">
+				<div class="wrap-col">
+					<h4>Open Daily</h4>
+					<p><span>mon.</span> 17:00 - 21:00</p>
+					<p><span>tue.-wed.</span> 16:30 – 21:00</p>
+					<p><span>thu.-sat.</span> 16:30 – 21:00</p>
+					<p><span>sun.</span> 11:00 – 21:00</p>
+					<p><span>Need help getting home?</span></br>
+					We will call a cab for you!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copyright">
+		<div class="wrapper">
+			Copyright 2015 - Designed by <a href="https://www.zerotheme.com">ZEROTHEME</a>
+			<ul class="quick-link f-right">
+				<li><a href="#">Privacy Policy</a></li>
+				<li><a href="#">Terms of Use</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+
+
+	<!-- js -->
+	<script src="{{ asset('public/frontend/js/classie.js')}}"></script>
+	<script src="{{ asset('public/frontend/js/demo.js')}}"></script>
+
+	<script src="{{ asset('public/frontend/js/jquery-1.11.3.min.js')}}"></script>
+	<script src="{{ asset('public/frontend/js/responsiveslides.min.js')}}"></script>
+	<script>
+	$(function () {
+	  // Slideshow 4
+	  $("#slider4").responsiveSlides({
+		auto: true,
+		pager: false,
+		nav: false,
+		speed: 500,
+		namespace: "callbacks",
+		before: function () {
+		  $('.events').append("<li>before event fired.</li>");
+		},
+		after: function () {
+		  $('.events').append("<li>after event fired.</li>");
+		}
+	  });
+	});
+	</script>
+</div>
+</body></html>
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//FrontEnd
+Route::get('/', function () {
+    return view('frontend.home');
+});
+
+Route::get('menu', function(){
+	return view('frontend.menu');
+})->name('menu');
+
+Route::get('location', function(){
+	return view('frontend.location');
+})->name('location');
+
+Route::get('blog', function(){
+	return view('frontend.archive');
+})->name('blog');
+
+Route::get('reservations', function(){
+	return view('frontend.reservation');
+})->name('reservation');
+
+Route::get('news', function(){
+	return view('frontend.news');
+})->name('news');
+
+Route::get('staff', function(){
+	return view('frontend.staff');
+})->name('staff');
+
+Route::get('gallery', function(){
+	return view('frontend.gallery');
+})->name('gallery');
+
+
+
+
+
+//BackEnd
+Auth::routes();
+
+Route::get('/admin', 'HomeController@index')->name('home');
